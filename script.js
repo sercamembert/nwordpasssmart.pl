8316c3ee12bc5e40b27808c45ac6ec480a9e4819
sercamembert <69466055+sercamembert@users.noreply.github.com>
2022-10-15 19:44:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/*let okno = document.getElementById('okno');
+let logo = document.getElementById('logo');
+let firstPlan = document.getElementById('first');
+let secoundPlan = document.getElementById('secound');
+logo.addEventListener('mouseover', (event) => {
+    okno.classList.add("window");
+    firstPlan.classList.add("move");
+    secoundPlan.classList.add("move");
+
+    
+
+});*/
+$("#logo").hover(function() {
+    $("#okno").addClass("window");
+    $("#first").addClass("move");
+    $("#secound").addClass("move");
+  }, function() {
+    $("#okno").removeClass("window");
+    $("#first").removeClass("move");
+    $("#secound").removeClass("move");
+  });
+  
+  $(".btn").hover(function() {
+    $("#okno").addClass("window");
+    $("#first").addClass("move");
+    $("#secound").addClass("move");
+  }, function() {
+    $("#okno").removeClass("window");
+    $("#first").removeClass("move");
+    $("#secound").removeClass("move");
+  });
+
